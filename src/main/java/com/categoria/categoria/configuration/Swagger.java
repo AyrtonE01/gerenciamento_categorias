838fc60abc3fc6b3a3ae466ae91d3fb7d9183b4a
AyrtonE01 <ayrton.enokida123@gmail.com>
2026-03-24 12:51:36 -0300

    Commit Entity

---
+++ src/main/java/com/categoria/categoria/configuration/Swagger.java
@@ -0,0 +1,7 @@
+package com.categoria.categoria.configuration;
+
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class Swagger {
+}
---
+++ src/main/java/com/categoria/categoria/entity/CategoriaEntity.java
@@ -0,0 +1,50 @@
+package com.categoria.categoria.entity;
+
+import jakarta.persistence.*;
+
+@Entity(name = "Categoria")
+@Table(name = "tb_categoria")
+public class CategoriaEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "nome_categoria")
+    private String nome;
+
+    @Column(name = "descricao_categoria")
+    private String descricao;
+
+    public CategoriaEntity() {}
+
+    public CategoriaEntity(Long id, String nome, String descricao) {
+        this.id = id;
+        this.nome = nome;
+        this.descricao = descricao;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+}
